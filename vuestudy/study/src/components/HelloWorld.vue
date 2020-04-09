b7decf12764a4c0cb344777cf132c93fb3027907
ToA <nansh9815@naver.com>
2020-04-09 16:00:29 +0900

    js jquery 04.09

---
+++ vuestudy/study/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <div v-if="childData.isLoading">    
+      <div v-for="aa in childData.aas" :key="aa">
+        {{aa}}
+      </div>
+    </div>
+    <div v-else>
+      <span>dddd</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    childData: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vuestudy/study/src/views/Study.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <hello-world :childData="parentData"></hello-world>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: 'study',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      parentData: {
+        aas: [
+        'vue',
+        '어렵다',
+        '이것도 쉬운 편이다',
+      ], //aa=[vue, 어렵다, 이것도 쉬운 편이다]
+      isLoading: false,
+      }
+    }
+  }
+  
+}
+</script>
