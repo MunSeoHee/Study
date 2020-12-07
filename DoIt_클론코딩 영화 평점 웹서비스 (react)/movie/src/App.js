7a14c90835459d3e050b01262bc470e0076b5f8a
ToA <nansh9815@naver.com>
2020-12-07 17:06:49 +0900

    20.12.07

--- DoIt_클론코딩 영화 평점 웹서비스 (react)/movie/src/App.js
+++ DoIt_클론코딩 영화 평점 웹서비스 (react)/movie/src/App.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading : true,
-    movies:[],
+    isLoading: true,
+    movies: [],
   };
-  componentDidMount(){
-    axios.get('https://yts.mx/api/v2/list_movies.json');
+  getMovies = async () => {
+    // const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    // console.log(movies.data.data.movies) 아래처럼 구조 분해 할당
+    const {
+      data: {
+        data: { movies }
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+
+    console.log(movies);
+
+    // this.setState({movies:movies}); 아래와 같이 축약 가능
+    this.setState({ movies, isLoading: false });
   }
-  render(){
-    const {isLoading}=this.state;
-  return <div>{isLoading?'loading':'ready'}</div>;
+
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+
+    return (<section className="container">{isLoading ? (<div className="loader"><span className="loader__text">Loading...</span></div>) :
+      (<div className="movies">{movies.map((movie) => {
+        console.log(movie);
+        return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />;
+      })}
+      </div>
+      )}
+    </section>);
   }
 }
 export default App;
